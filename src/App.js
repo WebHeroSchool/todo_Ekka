86675821a2c8422bc35f8e1a3d7d1762f7d3682d
Ekaterina Nikishina <ekkin3@gmail.com>
2020-03-29 16:18:50 +0300

    App.js changed

--- src/App.js
+++ src/App.js
@@ -2,15 +2,26 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const isLogo = true;
+const numberOfProject = 1;
+const desireToLearn = true;
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>
-          Hi, I'm a new project!
+        <h1 style={
+          { color: '#61dafb',
+            margin: 0
+          }
+        }>
+          Hi, It's my the {numberOfProject} project!
         </h1>
         <p>
-          I have a mesmerizing logo 
+          I did {numberOfProject - 1} react project before 
+        </p>
+        <p>
+          {isLogo && 'It has a mesmerizing logo'}
         </p>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -22,8 +33,15 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          {desireToLearn ? 'Learn React' : "Don't use the link"}
         </a>
+        <p>
+          undefined: {undefined},
+          null: {null},
+          false: {false},
+          true: {true},
+          NaN: {NaN}
+        </p>
       </header>
     </div>
   );
