b7c7a4c8d5c2a9459a52acd01663d29989f73c54
Ekaterina Nikishina <ekkin3@gmail.com>
2020-04-01 11:18:57 +0300

    new structure of the project

---
+++ src/components/App/Item/Item.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Item = () => (
+    <li>
+        Очень важное дело!
+    </li>    
+);
+
+export default Item;
---
+++ src/components/App/ItemList/ItemList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Item from '../Item/Item';
+
+const ItemList = () => (<ul>
+    <Item />
+    <Item />
+    <Item />
+    <Item />
+  </ul>)
+
+  export default ItemList;
+  
