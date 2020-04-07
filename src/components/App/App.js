93b175115ed0eb62c6410733d04c285da7f13ebc
Ekaterina Nikishina <ekkin3@gmail.com>
2020-04-07 10:36:54 +0300

    classnames added

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,20 +5,37 @@ import Footer from './Footer/Footer';
 import styles from './App.module.css';
 import '../../fonts.css';
 
-const todoItems = [
-  {id: 0, task: 'Сделать 19 урок'},
-  {id: 1, task: 'Оплатить интернет'},
-  {id: 2, task: 'Сварить кашу'},
-  {id: 3, task: 'Почитать книгу'}
-];
-const count = todoItems.length;
-
-const App = () => (
-  <main className={styles.wrap}>
+const App = () => {
+  const todoItems = [
+    {
+      id: 0, 
+      task: 'Сделать 19 урок',
+      isDone: true
+    },
+    {
+      id: 1, 
+      task: 'Оплатить интернет',
+      isDone: false
+    },
+    {
+      id: 2, 
+      task: 'Сварить кашу',
+      isDone: true
+    },
+    {
+      id: 3, 
+      task: 'Почитать книгу',
+      isDone: false
+    }
+  ];
+  const count = todoItems.length;
+  
+  return (<main className={styles.wrap}>
     <h1 className={styles.title}>Важные дела:</h1>    
     <InputItem />
     <ItemList todoItems={todoItems}/>
     <Footer count = {count} />
-  </main>)
+  </main>);
+}
 
 export default App;
--- src/components/App/ItemList/ItemList.js
+++ src/components/App/ItemList/ItemList.js
@@ -2,7 +2,8 @@ import React from 'react';
 import Item from '../Item/Item';
 
 const ItemList = ({todoItems}) => (<ul>
-    {todoItems.map(todoItem => <Item key={todoItem.id} task={todoItem.task}/>)}
+    {todoItems.map(todoItem => 
+      <Item key={todoItem.id} task={todoItem.task} isDone={todoItem.isDone} />)}
   </ul>)
 
   export default ItemList;
