67e78e25b10147d8308a7a09c25b8ed9493f292d
Ekaterina Nikishina <ekkin3@gmail.com>
2020-04-09 20:57:08 +0300

    component-class

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,45 +6,49 @@ import styles from './App.module.css';
 import '../../fonts.css';
 import { createMuiTheme } from '@material-ui/core/styles';
 
-const App = () => {
-  const todoItems = [
-    {
-      id: 0, 
-      task: 'Сделать 19 урок',
-      isDone: true
-    },
-    {
-      id: 1, 
-      task: 'Оплатить интернет',
-      isDone: false
-    },
-    {
-      id: 2, 
-      task: 'Сварить кашу',
-      isDone: true
-    },
-    {
-      id: 3, 
-      task: 'Почитать книгу',
-      isDone: false
-    }
-  ];
-  const count = todoItems.length;
-  
-  const theme = createMuiTheme({
-    palette: {
-      primary: {
-        main: '#6c5ce7'
+
+class App extends React.Component{
+  render() {
+    const todoItems = [
+      {
+        id: 0, 
+        task: 'Сделать 19 урок',
+        isDone: true
+      },
+      {
+        id: 1, 
+        task: 'Оплатить интернет',
+        isDone: false
+      },
+      {
+        id: 2, 
+        task: 'Сварить кашу',
+        isDone: true
+      },
+      {
+        id: 3, 
+        task: 'Почитать книгу',
+        isDone: false
       }
-    }
-  });
-  
-  return (<main className={styles.wrap}>
-    <h1 className={styles.title}>Важные дела:</h1>    
-    <InputItem theme={theme}/>
-    <ItemList todoItems={todoItems} theme={theme}/>
-    <Footer count = {count} />
-  </main>);
+    ];
+    
+    const count = todoItems.length;
+    
+    const theme = createMuiTheme({
+      palette: {
+        primary: {
+          main: '#6c5ce7'
+        }
+      }
+    });
+    
+    return (<main className={styles.wrap}>
+      <h1 className={styles.title}>Важные дела:</h1>    
+      <InputItem theme={theme}/>
+      <ItemList todoItems={todoItems} theme={theme}/>
+      <Footer count = {count} />
+    </main>);
+  }
 }
 
 export default App;
